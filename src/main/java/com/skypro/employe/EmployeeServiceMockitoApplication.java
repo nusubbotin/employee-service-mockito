e4eb45ee947ca73da4fb00bd404d100944c4645a
nusubbotin <nu.subbotin@ya.ru>
2022-11-20 14:59:59 +0300

    Инициализация проекта Книга сотрудников

---
+++ src/main/java/com/skypro/employe/EmployeeServiceMockitoApplication.java
@@ -0,0 +1,13 @@
+package com.skypro.employe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeServiceMockitoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeServiceMockitoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/skypro/employe/EmployeeServiceMockitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skypro.employe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeServiceMockitoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
